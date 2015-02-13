9ea08a7b9aa1bc9e3388ea4e03d510cba00ed11c
b.lobor <b.lobor@outlook.com>
2015-02-13 06:19:48 +0200

    Started working on resolving path of the dll.

---
+++ src/NWkHtmlToX/Core/Infrastructure/IPathResolver.cs
@@ -0,0 +1,14 @@
+﻿namespace NWkHtmlToX.Core.Infrastructure {
+     
+    /// <summary>
+    /// Implementations of this interface are used for resolving path.
+    /// </summary>
+    internal interface IPathResolver {
+
+        /// <summary>
+        /// Resolves a path.
+        /// </summary>
+        /// <returns>Resolved path.</returns>
+        string ResolvePath();
+    }
+}
