ad9ae9883b57a52e1884d48e4a4955dfb826912f
b.lobor <b.lobor@outlook.com>
2015-02-14 15:22:33 +0200

    Added some basic test

---
+++ test/NWkHtmlToX.Tests/Infrastructure/PathResolvers/PdfDllRegistryPathResolverTests.cs
@@ -0,0 +1,22 @@
+﻿using NWkHtmlToX.Infrastructure;
+using NWkHtmlToX.Infrastructure.PathResolvers;
+
+using Xunit;
+
+namespace NWkHtmlToX.Tests.Infrastructure.PathResolvers {
+    public class PdfDllRegistryPathResolverTests {
+
+        private IPathResolver _registryPathResolver;
+
+        public PdfDllRegistryPathResolverTests() {
+            _registryPathResolver = new PdfDllRegistryPathResolver();
+        }
+
+        [Fact]
+        public void DefaultResultShouldNotBeNull() {
+            var result = _registryPathResolver.ResolvePath();
+
+            Assert.NotNull(result);
+        }
+    }
+}
