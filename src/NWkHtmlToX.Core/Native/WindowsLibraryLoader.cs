75e52d3daa88fb21edf02d99f3afd03bc25a406f
b.lobor <b.lobor@outlook.com>
2015-02-15 22:08:47 +0200

    Added WindowsLibraryLoader class

---
+++ src/NWkHtmlToX.Core/Native/WindowsLibraryLoader.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Runtime.InteropServices;
+using NWkHtmlToX.Core.Native.PlatformApi;
+
+namespace NWkHtmlToX.Core.Native {
+    public class WindowsLibraryLoader : ILibraryLoader {
+
+        public IntPtr LoadLibrary(string dllPath) {
+            if (dllPath == null)
+                throw new ArgumentNullException(nameof(dllPath));
+            if (!File.Exists(dllPath))
+                throw new FileNotFoundException("Could not locate library.", dllPath);
+
+            var handler = WindowsApi.LoadLibrary(dllPath);
+
+            if (handler == IntPtr.Zero) {
+                var errorCode = Marshal.GetLastWin32Error();
+                var message = String.Format("Could not load library. Error code: {0}", errorCode.ToString(NumberFormatInfo.InvariantInfo));
+
+                throw new InvalidOperationException(message);
+            }
+
+            return handler;
+        }
+
+        public bool FreeLibrary(IntPtr handle) => handle != IntPtr.Zero && WindowsApi.FreeLibrary(handle);
+
+        public IntPtr GetProcAddress(IntPtr handle, string procedureName) {
+            if (handle == IntPtr.Zero)
+                throw new ArgumentException("Invalid library handle.", nameof(handle));
+            if (procedureName == null)
+                throw new ArgumentNullException(nameof(procedureName));
+
+            var procedure = WindowsApi.GetProcAddress(handle, procedureName);
+
+            if(procedure == IntPtr.Zero) {
+                var errorCode = Marshal.GetLastWin32Error();
+                var message = String.Format("Could not find procedure. Error code: {0}", errorCode.ToString(NumberFormatInfo.InvariantInfo));
+
+                throw new InvalidOperationException(message);
+            }
+
+            return procedure;
+        }
+    }
+}
---
+++ test/NWkHtmlToX.Core.Tests/Native/WindowsLibraryLoaderTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using NWkHtmlToX.Core.Native;
+using Xunit;
+
+namespace NWkHtmlToX.Core.Tests.Native {
+    public class WindowsLibraryLoaderTests {
+
+        private readonly ILibraryLoader _windowsLibraryLoader;
+
+        public WindowsLibraryLoaderTests() {
+            _windowsLibraryLoader = new WindowsLibraryLoader();
+        }
+
+        [Fact]
+        public void LoadLibraryMethodShoudThrowArgumentNullExceptionOnNullArgument() {
+            // Arrange
+            string testArgument = null;
+            Action testCode = () => _windowsLibraryLoader.LoadLibrary(testArgument);
+
+            // Act, Assert
+            Assert.Throws<ArgumentNullException>(testCode);
+        }
+
+        [Fact]
+        public void LoadLibraryMethodShoudThrowFileNotFoundExceptionIfLibraryWasNotFound() {
+            // Arrange
+            var testArgument = String.Format(@"C:\NotExistingFile_{0}.dll", DateTime.Now.Ticks.ToString(NumberFormatInfo.InvariantInfo));
+            Action testCode = () => _windowsLibraryLoader.LoadLibrary(testArgument);
+
+            // Act, Assert
+            Assert.Throws<FileNotFoundException>(testCode);
+        }
+
+        [Fact]
+        public void GetProcAddressMethodShoudThrowArgumentExceptionOnInvalidHandle() {
+            // Arrange
+            var handle = IntPtr.Zero;
+            var procedureName = String.Empty;
+            Action testCode = () => _windowsLibraryLoader.GetProcAddress(handle, procedureName);
+
+            // Act, Assert
+            Assert.Throws<ArgumentException>(nameof(handle), testCode);
+        }
+
+        [Fact]
+        public void GetProcAddressMethodShoudThrowArgumentNullExceptionOnNullProcedureNameArgument() {
+            // Arrange
+            var handle = new IntPtr(int.MinValue);
+            string procedureName = null;
+            Action testCode = () => _windowsLibraryLoader.GetProcAddress(handle, procedureName);
+
+            // Act, Assert
+            Assert.Throws<ArgumentNullException>(testCode);
+        }
+    }
+}
