aa66e594a52040fd220b6838c25a5d4b359f862d
b.lobor <b.lobor@outlook.com>
2015-03-01 11:19:31 +0200

    Removed target to ASPNETCORE framework.

--- src/NWkHtmlToX/Infrastructure/PathResolvers/RegistryBasePathResolver.cs
+++ src/NWkHtmlToX/Infrastructure/PathResolvers/RegistryBasePathResolver.cs
@@ -7,16 +7,11 @@ namespace NWkHtmlToX.Infrastructure.PathResolvers {
         protected const string WKHTMLTOX_DLL_PATH_REGISTRY_KEY = "DllPath";
 
         protected RegistryKey GetLocalMachineRegistryKey() {
-#if ASPNETCORE50
-            // Currently in .NET Core there are no possibility to check is system is 64 bit
-            return Registry.LocalMachine;
-#else
             var registryView = Environment.Is64BitOperatingSystem
                                                           ? RegistryView.Registry64
                                                           : RegistryView.Registry32;
 
             return RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, registryView);
-#endif
         }
 
         public abstract string ResolvePath();
