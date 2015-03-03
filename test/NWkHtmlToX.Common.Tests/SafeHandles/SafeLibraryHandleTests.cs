9332cdebfe2cdedee1f3ce175f546ba316c108d7
Bogdan Lobor <blobor@softserveinc.com>
2015-03-03 15:38:33 +0200

    Added test for SafeLibraryHandle class

---
+++ test/NWkHtmlToX.Common.Tests/SafeHandles/SafeLibraryHandleTests.cs
@@ -0,0 +1,18 @@
+﻿using NWkHtmlToX.Common.SafeHandles;
+using Xunit;
+
+namespace NWkHtmlToX.Common.Tests.SafeHandles {
+    public class SafeLibraryHandleTests {
+
+        [Fact]
+        public void SafeLibraryHandleShouldBeInvalidByDefault() {
+            // Arrange, Act
+            SafeLibraryHandle[] handles = { new SafeWindowsLibraryHandle() };
+
+            // Assert
+            foreach (var handle in handles) {
+                Assert.True(handle.IsInvalid);
+            }
+        }
+    }
+}
