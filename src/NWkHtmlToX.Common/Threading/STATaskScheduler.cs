e86745759c006beffd9a1a62bf5395cc6db02d17
Bogdan Lobor <blobor@softserveinc.com>
2015-03-04 20:04:51 +0200

    Added STATaskScheduler class.

---
+++ src/NWkHtmlToX.Common/Threading/STATaskScheduler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace NWkHtmlToX.Common.Threading {
+    internal sealed class STATaskScheduler : TaskScheduler {
+
+        private readonly IThreadFactory _threadFactory;
+
+        public STATaskScheduler(IThreadFactory factory) {
+            if(factory == null) throw new ArgumentNullException(nameof(factory));
+
+            _threadFactory = factory;
+        }
+
+        protected override void QueueTask(Task task) {
+            throw new System.NotImplementedException();
+        }
+
+        protected override bool TryExecuteTaskInline(Task task, bool taskWasPreviouslyQueued) {
+            throw new System.NotImplementedException();
+        }
+
+        protected override IEnumerable<Task> GetScheduledTasks() {
+            throw new System.NotImplementedException();
+        }
+    }
+}
