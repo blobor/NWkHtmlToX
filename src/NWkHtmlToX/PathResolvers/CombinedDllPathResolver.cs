1a354185fb0b219121d13e85afc01587cf997206
Bogdan Lobor <b.lobor@outlook.com>
2015-03-11 06:27:33 +0200

    Added CombinedDllPathResolver class.

---
+++ src/NWkHtmlToX/PathResolvers/CombinedDllPathResolver.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using System.Linq;
+using NWkHtmlToX.Common.Utilities;
+
+namespace NWkHtmlToX.PathResolvers {
+    public class CombinedDllPathResolver : IPathResolver {
+
+        protected IPathResolver[] PathResolvers;
+
+        public CombinedDllPathResolver(params IPathResolver[] pathResolvers) {
+            Guard.ArgumentNotNull(pathResolvers, nameof(pathResolvers));
+
+            PathResolvers = pathResolvers;
+        }
+
+        public string ResolvePath() {
+            string result = null;
+            foreach (var pathResolver in PathResolvers.Where(resolver => resolver != null)) {
+                result = pathResolver.ResolvePath();
+                if (File.Exists(result)) {
+                    break;
+                }
+            }
+            return result;
+        }
+    }
+}
