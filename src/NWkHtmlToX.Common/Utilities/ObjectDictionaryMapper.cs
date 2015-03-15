3b9102435ae0d3d513006324dfdf987e01c28af0
Bogdan Lobor <b.lobor@outlook.com>
2015-03-15 23:31:35 +0200

    Added ObjectDictionaryMapper class.

---
+++ src/NWkHtmlToX.Common/Utilities/ObjectDictionaryMapper.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace NWkHtmlToX.Common.Utilities {
+    internal static class ObjectDictionaryMapper {
+
+        private const BindingFlags DEFAULT_BINDING_FLAGS = BindingFlags.Instance |
+                                                           BindingFlags.GetProperty |
+                                                           BindingFlags.Public;
+
+        internal static IDictionary<string, string> GetDictionary<T>(T instance) {
+            return GetDictionary(instance, DEFAULT_BINDING_FLAGS);
+        }
+
+        internal static IDictionary<string, string> GetDictionary<T>(T instance, BindingFlags bindingFlags) {
+            Guard.ArgumentNotNull(instance, nameof(instance));
+
+            IDictionary<string, string> result = new Dictionary<string, string>();
+
+            GetProperties(result, instance, bindingFlags);
+
+            return result;
+        }
+
+        private static void GetProperties<T>(IDictionary<string, string> properties, T instance, BindingFlags bindingFlags, string prefix = null) {
+            foreach (var property in instance.GetType().GetProperties(bindingFlags)) {
+                var value = property.GetValue(instance);
+                if (value == null) continue;
+
+                var propertyType = property.PropertyType;
+                var propertyName = prefix == null ? ToCamelCase(property.Name) : String.Concat(prefix, Type.Delimiter, ToCamelCase(property.Name));
+
+                if ((propertyType.IsValueType && !value.Equals(Activator.CreateInstance(propertyType))) || propertyType == typeof(string)) {
+                    properties.Add(propertyName, propertyType == typeof(bool) ? value.ToString().ToLowerInvariant() : value.ToString());
+                } else {
+                    GetProperties(properties, value, bindingFlags, propertyName);
+                }
+            }
+        }
+
+        private static string ToCamelCase(string value) {
+            return String.IsNullOrEmpty(value) ? value : String.Concat(Char.ToLowerInvariant(value[0]), value.Substring(1));
+        }
+    }
+}
---
+++ test/NWkHtmlToX.Common.Tests/Utilities/ObjectDictionaryMapperTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using NWkHtmlToX.Common.Utilities;
+using Xunit;
+
+namespace NWkHtmlToX.Common.Tests.Utilities {
+    public class ObjectDictionaryMapperTests {
+
+        [Fact]
+        public void GetDictionary_ShouldReturnDictionaryOfObjectProperties() {
+
+            // Arrange
+            var obj = new {
+                TestString = "ABC",
+                TestInt32 = 11,
+                TestDateTime = DateTime.Now,
+                TestInnerObject = new {
+                    TestString = "ABC",
+                    TestInt32 = 11
+                }
+            };
+            IDictionary<string, string> expectedResult = new Dictionary<string, string> {
+                ["testString"] =  "ABC",
+                ["testInt32"] = "11",
+                ["testDateTime"] = DateTime.Now.ToString(),
+                ["testInnerObject.testString"] = "ABC",
+                ["testInnerObject.testInt32"] = "11"
+            };
+
+            // Act
+            var actualResult = ObjectDictionaryMapper.GetDictionary(obj);
+
+            // Assert
+            Assert.Equal(expectedResult, actualResult);
+        }
+
+    }
+}
