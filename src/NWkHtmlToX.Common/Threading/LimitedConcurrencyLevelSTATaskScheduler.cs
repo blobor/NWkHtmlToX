88b899ab15fb6cbe2fc1bb685314589b29801ee7
Bogdan Lobor <b.lobor@outlook.com>
2015-03-17 07:44:17 +0200

    Started implementation of the new STA task scheduler.

---
+++ src/NWkHtmlToX.Common/Threading/LimitedConcurrencyLevelSTATaskScheduler.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using NWkHtmlToX.Common.Utilities;
+
+namespace NWkHtmlToX.Common.Threading {
+    internal sealed class LimitedConcurrencyLevelSTATaskScheduler : TaskScheduler {
+
+        private readonly ConcurrentQueue<Task> _tasks;
+
+        public LimitedConcurrencyLevelSTATaskScheduler(int maximumConcurrencyLevel) {
+            if (maximumConcurrencyLevel < 1) throw new ArgumentOutOfRangeException(nameof(maximumConcurrencyLevel));
+
+            MaximumConcurrencyLevel = maximumConcurrencyLevel;
+            _tasks = new ConcurrentQueue<Task>();
+        }
+
+        public override int MaximumConcurrencyLevel { get; }
+
+        protected override void QueueTask(Task task) {
+            Guard.ArgumentNotNull(task, nameof(task));
+
+            _tasks.Enqueue(task);
+        }
+
+        protected override bool TryExecuteTaskInline(Task task, bool taskWasPreviouslyQueued) {
+            Guard.ArgumentNotNull(task, nameof(task));
+
+            throw new NotImplementedException();
+        }
+
+        protected override IEnumerable<Task> GetScheduledTasks() {
+            return _tasks;
+        }
+    }
+}
