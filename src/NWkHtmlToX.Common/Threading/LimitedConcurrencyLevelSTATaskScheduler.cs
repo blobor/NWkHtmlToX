1bd493537daf440fe7530962292ff4e307926cf3
Bogdan Lobor <blobor@softserveinc.com>
2015-03-17 18:23:36 +0200

    Implemented STATaskScheduler with limited concurrency level.

--- src/NWkHtmlToX.Common/Threading/LimitedConcurrencyLevelSTATaskScheduler.cs
+++ src/NWkHtmlToX.Common/Threading/LimitedConcurrencyLevelSTATaskScheduler.cs
@@ -8,13 +8,18 @@ using NWkHtmlToX.Common.Utilities;
 namespace NWkHtmlToX.Common.Threading {
     internal sealed class LimitedConcurrencyLevelSTATaskScheduler : TaskScheduler {
 
+        private int _numberOfCurrentlyRunningThreads;
         private readonly ConcurrentQueue<Task> _tasks;
+        private readonly IThreadFactory _threadFactory;
+        private readonly ThreadLocal<bool> _isCurrentThreadProcessingTask = new ThreadLocal<bool>(() => false);
 
         public LimitedConcurrencyLevelSTATaskScheduler(int maximumConcurrencyLevel) {
-            if (maximumConcurrencyLevel < 1) throw new ArgumentOutOfRangeException(nameof(maximumConcurrencyLevel));
+            ThrowIf.Argument.IsOutOfRange(maximumConcurrencyLevel, nameof(maximumConcurrencyLevel), 1, Int32.MinValue);
 
-            MaximumConcurrencyLevel = maximumConcurrencyLevel;
+            _numberOfCurrentlyRunningThreads = 0;
             _tasks = new ConcurrentQueue<Task>();
+            _threadFactory = new STAThreadFactory();
+            MaximumConcurrencyLevel = maximumConcurrencyLevel;
         }
 
         public override int MaximumConcurrencyLevel { get; }
@@ -23,16 +28,42 @@ namespace NWkHtmlToX.Common.Threading {
             ThrowIf.Argument.IsNull(task, nameof(task));
 
             _tasks.Enqueue(task);
+
+            if (_numberOfCurrentlyRunningThreads < MaximumConcurrencyLevel) {
+                Interlocked.Increment(ref _numberOfCurrentlyRunningThreads);
+                _threadFactory.Create(ExecuteWork).Start();
+            }
         }
 
         protected override bool TryExecuteTaskInline(Task task, bool taskWasPreviouslyQueued) {
             ThrowIf.Argument.IsNull(task, nameof(task));
 
-            throw new NotImplementedException();
+            // If this thread isn't already processing a task, we don't support inlining
+            if (_isCurrentThreadProcessingTask.Value) return false;
+
+            // check if CurrentThread have appropriate ApartmentState
+            if (Thread.CurrentThread.GetApartmentState() != ApartmentState.STA) return false;
+
+            // Try to run the task.
+            return taskWasPreviouslyQueued ? TryDequeue(task) && TryExecuteTask(task) : TryExecuteTask(task);
         }
 
         protected override IEnumerable<Task> GetScheduledTasks() {
-            return _tasks;
+            return _tasks.ToArray(_tasks.Count);
+        }
+
+        private void ExecuteWork() {
+            _isCurrentThreadProcessingTask.Value = true;
+            try {
+                while (!_tasks.IsEmpty) {
+                    Task queuedTask;
+                    if (_tasks.TryDequeue(out queuedTask)) {
+                        TryExecuteTask(queuedTask);
+                    }
+                }
+            } finally {
+                _isCurrentThreadProcessingTask.Value = false;
+            }
         }
     }
 }
