96e4707197adeba85b5b0f9eb7b8ff08a1d5e89d
Bogdan Lobor <blobor@softserveinc.com>
2015-03-17 18:20:57 +0200

    Added extension for fast Array creating of IEnumerable`T types.

---
+++ src/NWkHtmlToX.Common/Utilities/EnumerableExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace NWkHtmlToX.Common.Utilities {
+    internal static class EnumerableExtensions {
+
+        [SuppressMessage("ReSharper", "PossibleMultipleEnumeration")]
+        internal static T[] ToArray<T>(this IEnumerable<T> source, int count) {
+            ThrowIf.Argument.IsNull(source, nameof(source));
+            ThrowIf.Argument.IsOutOfRange(count, nameof(count), 0, Int32.MaxValue);
+
+            var index = 0;
+            var result = new T[count];
+            foreach (var item in source) {
+                result[index++] = item;
+            }
+            return result;
+        }
+    }
+}
