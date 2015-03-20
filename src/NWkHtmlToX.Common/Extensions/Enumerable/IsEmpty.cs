fbbb300a0e3411952e64f7236a96c706ea5e4019
Bogdan Lobor <b.lobor@outlook.com>
2015-03-21 01:31:12 +0200

    Added overloads for "IsEmpty" method. (reverted from commit 2dfadb0ffbfbbaaec97118cb8484800563a32f85)

--- src/NWkHtmlToX.Common/Extensions/Enumerable/IsEmpty.cs
+++ src/NWkHtmlToX.Common/Extensions/Enumerable/IsEmpty.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 
 namespace NWkHtmlToX.Common.Extensions.Enumerable {
@@ -7,14 +6,6 @@ namespace NWkHtmlToX.Common.Extensions.Enumerable {
 
         internal static bool IsEmpty<T>(this IEnumerable<T> source) {
             return !source.Any();
-        }
-
-        internal static bool IsEmpty(this ICollection source) {
-            return source.Count < 1;
-        }
-
-        internal static bool IsEmpty<T>(this ICollection<T> source) {
-            return source.Count < 1;
         }
     }
 }
