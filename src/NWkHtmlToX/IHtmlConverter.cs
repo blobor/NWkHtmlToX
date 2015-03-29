4d509329c3f7e7f41c4074504320dc7c5e7db9db
Bogdan Lobor <b.lobor@outlook.com>
2015-03-29 19:12:53 +0300

    Added base interface for html converter.

---
+++ src/NWkHtmlToX/IHtmlConverter.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NWkHtmlToX {
+    public interface IHtmlConverter : IDisposable {
+        Task<byte[]> ConvertAsync();
+        Task<byte[]> ConvertAsync(CancellationToken cancellationToken);
+    }
+}
