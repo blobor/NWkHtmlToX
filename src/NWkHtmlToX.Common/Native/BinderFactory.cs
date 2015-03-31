9ee15a8641ebab23bb091a2c3d0b144ebfa20e2c
Bogdan Lobor <b.lobor@outlook.com>
2015-03-31 22:06:41 +0300

    Added BinderFacory object.

---
+++ src/NWkHtmlToX.Common/Native/BinderFactory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using NWkHtmlToX.Common.SafeHandles;
+
+namespace NWkHtmlToX.Common.Native {
+    internal class BinderFactory {
+
+        private readonly ILibraryLoader _libraryLoader;
+
+        internal BinderFactory(ILibraryLoader libraryLoader) {
+            _libraryLoader = libraryLoader;
+        }
+
+        internal T BindToDelegates<T>(SafeLibraryHandle libraryHandle) where T : class, new() {
+            var result = new T();
+            var delegateType = typeof (Delegate);
+
+            foreach (var propertyInfo in typeof(T).GetTypeInfo().DeclaredProperties.Where(property => delegateType.IsAssignableFrom(property.PropertyType)
+                                                                                                   && property.CanWrite)) {
+                IntPtr procedure;
+                if (_libraryLoader.TryGetProcAddress(libraryHandle, propertyInfo.PropertyType.Name, out procedure)) {
+                    propertyInfo.SetValue(result, Marshal.GetDelegateForFunctionPointer(procedure, propertyInfo.PropertyType));
+                }
+            }
+
+            return result;
+        }
+    }
+}
