80bb1395665f24f47223fba6e82dc348dbb66f17
Bogdan Lobor <b.lobor@outlook.com>
2015-04-14 21:18:05 +0300

    Minor changes in BinderFactory.

--- src/NWkHtmlToX.Common/Native/BinderFactory.cs
+++ src/NWkHtmlToX.Common/Native/BinderFactory.cs
@@ -13,12 +13,13 @@ namespace NWkHtmlToX.Common.Native {
             _libraryLoader = libraryLoader;
         }
 
-        internal T BindToDelegates<T>(SafeLibraryHandle libraryHandle) where T : class, new() {
+        internal T MapDelegates<T>(SafeLibraryHandle libraryHandle) where T : class, new() {
             var result = new T();
+            var objectTypeInfo = typeof (T).GetTypeInfo();
             var delegateType = typeof (Delegate);
 
-            foreach (var propertyInfo in typeof(T).GetTypeInfo().DeclaredProperties.Where(property => delegateType.IsAssignableFrom(property.PropertyType)
-                                                                                                   && property.CanWrite)) {
+            foreach (var propertyInfo in objectTypeInfo.DeclaredProperties.Where(property => delegateType.IsAssignableFrom(property.PropertyType)
+                                                                                          && property.CanWrite)) {
                 IntPtr procedure;
                 if (_libraryLoader.TryGetProcAddress(libraryHandle, propertyInfo.PropertyType.Name, out procedure)) {
                     propertyInfo.SetValue(result, Marshal.GetDelegateForFunctionPointer(procedure, propertyInfo.PropertyType));
